5559a02ba1cafe61072fe19b59a69b1a956b76cd
hadeeny <oluwasegunadeniyi064@gmail.com>
2022-11-21 13:18:16 +0100

    fin

--- pages/index.jsx
+++ pages/index.jsx
@@ -28,16 +28,13 @@ export default function Home() {
         <WorkExperience />
       </section>
 
-      {/* 
-
-
       <section id="skills" className="snap-start">
         <Skills />
       </section>
 
       <section id="projects" className="snap-start">
         <Projects />
-      </section> */}
+      </section>
     </div>
   );
 }
