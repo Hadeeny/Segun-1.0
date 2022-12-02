d32c821cc761ce3472703d602aa1e415e4a1a217
Oluwasegun Michael Adeniyi <63579745+Hadeeny@users.noreply.github.com>
2022-12-02 19:20:17 +0100

    Color change

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -17,11 +17,11 @@ const Hero = () => {
         className="relative rounded-full w-32 h-32 mx-auto object-cover"
       />
       <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-400 mb-4 tracking-[1rem]">
+        <h2 className="text-sm uppercase text-[#e7ffe8] mb-4 tracking-[1rem]">
           Software Developer
         </h2>
         <h1>
-          <span className="text-2xl lg:6xl text-gray-200 tracking-widest font-semibold">{text}</span>
+          <span className="text-2xl lg:6xl text-[#e7ffe8] tracking-widest font-semibold">{text}</span>
           <Cursor cursorColor="red" />
         </h1>
         <div className="pt-5 space-x-2 md:space-x-5">
