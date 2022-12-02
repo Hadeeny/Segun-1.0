ed4a16ffc6cb13a27b4432372ad98dbc024a88ab
Oluwasegun Michael Adeniyi <63579745+Hadeeny@users.noreply.github.com>
2022-12-02 19:09:33 +0100

    Color change

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -60,7 +60,7 @@ const Projects = () => {
         className="h-screen relative flex overflow-hidden flex-col text-left 
         md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
       >
-        <h3 className="absolute top-24 z-[400] uppercase tracking-[20px] text-gray-300 text-2xl">
+        <h3 className="absolute top-24 z-[400] uppercase tracking-[20px] text-[#e7ffe8] text-2xl">
           Projects
         </h3>
         <div className="relative top-[3rem] z-[300] w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory">
